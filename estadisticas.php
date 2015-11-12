bc686abcaee8ee0b5252ca1b50efd680dbad3eb0
sergio <travian-arnoll@hotmail.com>
2015-11-12 16:06:10 +0100

    ya esta

--- estadisticas.php
+++ estadisticas.php
@@ -12,59 +12,15 @@
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="Scripts/bootstrap.min.js"></script>
 
-    <style type="text/css">
-			body {
-			padding-top: 20px;
-			}
-			div {
-			margin-bottom: 150px;
-			margin-left: 150px;
-			margin-right: 150px;
-			}
-			.jumbotron{
-			text-align: center;
-			}
-			button{
-			margin: 5px;
-			}
-			table {
-				font-family: "Lucida Sans Unicode", "Lucida Grande", Sans-Serif;
-    			font-size: 12px;
-    			width: 480px; 
-    			text-align: center;    
-    			border-collapse: collapse;
-    			margin-bottom: 150px;
-				margin-left: 150px;
-				margin-right: 150px;
-    			}
-
-			th {
-				font-size: 13px;     
-				font-weight: normal;     
-				padding: 8px;     
-				background: #b9c9fe;
-			    border-top: 4px solid #aabcfe;    
-			    border-bottom: 1px solid #fff; 
-			    color: #039;
-			    }
-
-			td {
-				padding: 8px;     
-				background: #e8edff;     
-				border-bottom: 1px solid #fff;
-			    color: #669;    
-			    border-top: 1px solid transparent;
-			    }
-
-			
-		</style>
+    
 </head>
 <body>
 <div class="container">
 <div class="jumbotron">
 	<h1>Estadisticas</h1>
 </div>
-<div>
+<table class="table"><tr><td>Origen</td><td>Visitas</td><td>Porcentaje</td></tr>
+
 	<?php
 
 		$mysqli = new mysqli('mysql.hostinger.es','u773293184_sergi','sergio','u773293184_user',3306);
@@ -73,7 +29,7 @@
 
 		$numv = $mysqli->query($query);
 
-		echo "<table border=1 ><tr><td>Origen</td><td>Visitas</td><td>Porcentaje</td></tr>";
+	
 
 		while( $row = $numv->fetch_array(MYSQL_BOTH)){
 			echo "<tr><td>".$row["destino"]."</td><td>".$row["visitas"]."</td><td>".$row["porcentaje"]."</td></tr>";
@@ -81,7 +37,7 @@
 		echo "</table>";
 
 	?>
-</div>
+
 </div>
 
 </body>
